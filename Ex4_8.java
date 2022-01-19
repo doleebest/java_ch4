af36af110693d3e512d4a4c1de8b4c79c097eafb
doleebest <90204371+doleebest@users.noreply.github.com>
2022-01-19 22:29:54 +0900

    Update Ex4_8.java

--- Ex4_8.java
+++ Ex4_8.java
@@ -1,7 +1,18 @@
 class Ex4_8 {
 	public static void main(String args[]) {
-		for (int i = 1; i <= 3; i++) { // °ýÈ£{}¾ÈÀÇ ¹®ÀåÀ» 3¹ø ¹Ýº¹
-			System.out.println("Hello");
-		}
+//		for (int i = 1; i <= 3; i++) { // ê´„í˜¸{}ì•ˆì˜ ë¬¸ìž¥ì„ 3ë²ˆ ë°˜ë³µ
+//			System.out.println("Hello");
+			
+//		for (int i=1; i<10; i=i+2)
+//			System.out.println("hello");
+	
+//		for (int i=1,j=10; i<=10; i++, j--)
+//			System.out.println("i="+i+ " ,j="+j);
+	
+		int i=10;
+		for (;i>=0;i--)
+			System.out.println(i);
+	
+	
 	}
-}
+	}
